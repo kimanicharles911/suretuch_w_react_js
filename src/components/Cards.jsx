a883431afcebc9889b887efb2f8d7e407dbcadeb
kimanicharles911 <kimanicharles911@gmail.com>
2021-02-10 13:30:48 +0300

    card section finished

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,11 @@
 import CardItem from "./CardItem";
 import "./Cards.css"; 
-import firstImg from "../images/img-9.jpg"
+import firstImg from "../images/img-9.jpg";
+import secondImg from "../images/img-2.jpg";
+import thirdImg from "../images/img-3.jpg";
+import fourthImg from "../images/img-4.jpg";
+import fifthImg from "../images/img-8.jpg";
+
 
 function Cards(){
   return (
@@ -10,6 +15,12 @@ function Cards(){
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem src={firstImg} text="Explore the deep hidden waterfall inside the Amazon Jungle" label="Adventure" path="/services"/>
+            <CardItem src={secondImg} text="Travel through the Islands of Bali in a Private Cruise" label="Luxury" path="/services"/>
+          </ul>
+          <ul className="cards__items">
+            <CardItem src={thirdImg} text="Explore the deep hidden waterfall inside the Amazon Jungle" label="Adventure" path="/services"/>
+            <CardItem src={fourthImg} text="Travel through the Islands of Bali in a Private Cruise" label="Luxury" path="/services"/>
+            <CardItem src={fifthImg} text="Travel through the Islands of Bali in a Private Cruise" label="Luxury" path="/services"/>
           </ul>
         </div>
       </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import bgVid from "../videos/video-2.mp4";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src={bgVid} autoPlay loop muted />
+      {/* <video src={bgVid} autoPlay loop muted /> */}
       <h1>Sure-Tuch Creations</h1>
       <p>Art in the Product</p>
       <div className="hero-btns">
