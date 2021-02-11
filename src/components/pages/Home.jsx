508b70745382208bc7a0f7063aae7b8c32b2ed50
kimanicharles911 <kimanicharles911@gmail.com>
2021-02-11 14:55:51 +0300

    complete footer prototype

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../HeroSection.jsx";
 import Cards from "../Cards.jsx";
+import Footer from "../Footer.jsx";
 
 function Home () {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   )
 }
